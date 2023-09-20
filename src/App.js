6ec7dcbefbe1db904f6ec5b9373e33f8e3cea188
samhalsall23 <54165031+samhalsall23@users.noreply.github.com>
2023-09-20 01:24:55 +0000

    7 - prop drilling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
-import Final from "./tutorial/5-useRef/final/1-useRef-basics";
+import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
+import Final from "./tutorial/7-prop-drilling/final/1-prop-drilling";
 
 function App() {
   return (
