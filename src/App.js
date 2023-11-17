89c455870d79302e88342672753c459c447613b7
samhalsall23 <54165031+samhalsall23@users.noreply.github.com>
2023-11-17 03:34:40 +0000

    finished last tutorial !

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/10-prop-types/setup/index";
+import Setup from "./tutorial/12-memo-useMemo-useCallback/setup/index";
 import Final from "./tutorial/9-custom-hooks/final/1-fetch-example";
 
 function App() {
--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -1,48 +1,92 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
+import React, { useState, useEffect, useCallback, useMemo } from "react";
+import { useFetch } from "../../9-custom-hooks/final/2-useFetch";
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/javascript-store-products'
+const url = "https://course-api.com/javascript-store-products";
 
 // every time props or state changes, component re-renders
 
+//Reactmemo is looking to see if props change whereas useMemo is looking at exact value
+
+const calculateMostExpensive = (data) => {
+  return (
+    data.reduce((maxPrice, item) => {
+      const price = item.fields.price;
+      if (price > maxPrice) {
+        maxPrice = price;
+      }
+      return maxPrice;
+    }, 0) / 100
+  );
+};
+
 const Index = () => {
-  const { products } = useFetch(url)
-  const [count, setCount] = useState(0)
+  const { products } = useFetch(url);
+  const [count, setCount] = useState(0);
+  const [cart, setCart] = useState(0);
+
+  // useCallback used to check if the value for function has changed
+  // same as reactmemo but for a function
+  const addToCart = useCallback(() => {
+    setCart(cart + 1);
+  }, [cart]); // adding cart to say that when we change cart we create this function
+  // without it when we add to cart it will not increment
+
+  const mostExpensive = useMemo(
+    () => calculateMostExpensive(products),
+    [products]
+  );
 
   return (
     <>
       <h1>Count : {count}</h1>
-      <button className='btn' onClick={() => setCount(count + 1)}>
+      <button className="btn" onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{ marginTop: "3rem" }}>cart:{cart}</h1>
+      <h1> Most Expensive : ${mostExpensive}</h1>
+      <BigList products={products} addToCart={addToCart} />
     </>
-  )
-}
+  );
+};
 
-const BigList = ({ products }) => {
+// Reactmemo is checking what is the value,
+// knows it does not change so does not rerender it
+const BigList = React.memo(({ products, addToCart }) => {
+  useEffect(() => {
+    console.log("big list called");
+  });
   return (
-    <section className='products'>
+    <section className="products">
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return (
+          <SingleProduct
+            key={product.id}
+            {...product}
+            addToCart={addToCart}
+          ></SingleProduct>
+        );
       })}
     </section>
-  )
-}
+  );
+});
 
-const SingleProduct = ({ fields }) => {
-  let { name, price } = fields
-  price = price / 100
-  const image = fields.image[0].url
+const SingleProduct = ({ fields, addToCart }) => {
+  useEffect(() => {
+    console.log("single product called");
+  });
+  let { name, price } = fields;
+  price = price / 100;
+  const image = fields.image[0].url;
 
   return (
-    <article className='product'>
+    <article className="product">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>add to cart</button>
     </article>
-  )
-}
-export default Index
+  );
+};
+export default Index;
